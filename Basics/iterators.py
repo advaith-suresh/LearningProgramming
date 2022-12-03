95f386babe6f7e0ece996128d9186ca18f85d1f7
Advaith Suresh <advaith.srs@gmail.com>
2022-12-03 17:15:36 +0530

    Added test file and a file with iterator concepts

---
+++ Basics/iterators.py
@@ -0,0 +1,62 @@
+'''
+Learning: some properties of iterators and iterables
+'''
+
+class Queue:
+    def __init__(self):
+        self.n = 0
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        self.n += 1
+        if self.n <= 20:
+            return self.n
+        else:
+            raise StopIteration
+
+shop = Queue()
+
+# making a loop using iterators
+'''
+line = shop.__iter__()
+
+while 1:
+    try:
+        print(line.__next__())
+    except StopIteration:
+        print("Have to stop here sorry :(")
+        break
+'''
+
+# making 2 iterator objects to see how they interact
+# ans: since line 2 is still SHOP the __iter__ command brings n of SHOP to 0, which brings n of LINE1 to 0
+'''
+line1 = shop.__iter__()
+print(line1.__next__())
+print(line1.__next__())
+
+line2 = shop.__iter__()
+print(line1.__next__())
+print(line1.__next__())
+'''
+
+# do we even need iter
+# ans: not if we use it like below
+'''
+while 1:
+    try:
+        print(shop.__next__())
+    except StopIteration:
+        print("Have to stop here sorry :(")
+        break
+'''
+
+# to use the class as an iterable like below we need the __iter__ method
+# else TypeError: 'Queue' object is not iterable
+'''
+for i in shop:
+    print(i)
+'''
---
+++ test.py
@@ -0,0 +1,4 @@
+'''
+The purpose of this file is to test/goof around with concepts without having to make new file every time
+'''
+
